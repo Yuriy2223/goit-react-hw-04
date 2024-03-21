c616e8734251763f367ffedb49458d3fabebc78a
Yuriy2223 <yuriy.shukan@gmail.com>
2024-03-21 18:26:51 +0200

    finish

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,13 @@
 import styles from "./ImageCard.module.css";
 
 const ImageCard = ({ image, onOpen }) => (
-  <div className={styles.box} onClick={() => onOpen(image)}>
-    <img className={styles.img} src={image.urls.small} alt={image.alt_description} />
+  <div className={styles.box}>
+    <img
+      className={styles.img}
+      src={image.urls.small}
+      alt={image.alt_description}
+      onClick={() => onOpen(image)}
+    />
   </div>
 );
 
--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,6 +1,22 @@
+// import styles from "./LoadMoreBtn.module.css";
+
+// const LoadMoreBtn = ({ onLoadMore }) => {
+//   return (
+//     <button onClick={onLoadMore} className={styles.loadMoreBtn}>
+//       Load more
+//     </button>
+//   );
+// };
+
+// export default LoadMoreBtn;
+
 import styles from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = ({ onLoadMore }) => {
+const LoadMoreBtn = ({ onLoadMore, hasMore }) => {
+  if (!hasMore) {
+    return null;
+  }
+
   return (
     <button onClick={onLoadMore} className={styles.loadMoreBtn}>
       Load more
